9d33fb0fd85f7f715343d13a9d652414027eea66
Theo <tgsb@cin.ufpe.br>
2025-03-25 03:08:33 -0300

    Testes unitarios finalizados

--- src/multer/multer.js
+++ src/multer/multer.js
@@ -2,21 +2,17 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-// Usando __dirname diretamente
 const __dirname = path.resolve();
 
-// Diretórios
 const defaultUsersDirectory = path.join(__dirname, "uploads/usersTemplates");
 const testUsersDirectory = path.join(__dirname, "__tests__/int_tests/temp_uploads");
 
-// Função para garantir que o diretório de teste exista
 const ensureTestDirectoryExists = () => {
   if (process.env.NODE_ENV === "test" && !fs.existsSync(testUsersDirectory)) {
     fs.mkdirSync(testUsersDirectory, { recursive: true });
   }
 };
 
-// Chama a função para garantir o diretório antes de iniciar os uploads
 ensureTestDirectoryExists();
 
 const publicStorage = multer.diskStorage({
@@ -30,7 +26,6 @@ const publicStorage = multer.diskStorage({
 
 const usersStorage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // Se o ambiente é de teste, usa o diretório de testes
     const uploadDirectory =
       process.env.NODE_ENV === "test"
         ? testUsersDirectory
@@ -58,13 +53,13 @@ const fileFilter = (req, file, cb) => {
 const publicUpload = multer({
   storage: publicStorage,
   fileFilter: fileFilter,
-  limits: { fileSize: 1024 * 1024 * 5 }, // Limite de 5MB
+  limits: { fileSize: 1024 * 1024 * 5 },
 });
 
 const usersUpload = multer({
   storage: usersStorage,
   fileFilter: fileFilter,
-  limits: { fileSize: 1024 * 1024 * 5 }, // Limite de 5MB
+  limits: { fileSize: 1024 * 1024 * 5 },
 });
 
 export { publicUpload, usersUpload };
